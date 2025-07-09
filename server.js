03495a0f387e5e7ee85e920b11647c803be851ed
SrushtiShaha <shahasrushti599@gmail.com>
2025-07-09 08:33:24 +0800

    Updated backend

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ if (!MONGO_URI) {
   process.exit(1);
 }
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(MONGO_URI).then(() => {
   console.log('✅ MongoDB connected');
 }).catch(err => {
   console.error('❌ MongoDB connection error:', err);
